3bb96bd0951e67da2066edecf67bfa3ca7861cc4
Denys Vitali <denys@denv.it>
2022-01-25 03:54:00 +0100

    wip: new endpoints

---
+++ src/api/app_id.rs
@@ -0,0 +1,69 @@
+use std::fmt::format;
+use std::io::Cursor;
+use chrono::Utc;
+use hmac::{Hmac, Mac};
+use crate::api::{SECRET, USER_AGENT};
+use sha2::Sha256;
+
+type HmacSha256 = Hmac<Sha256>;
+use byteorder::{BigEndian, ReadBytesExt};
+
+fn calculate_hmac(s: &str) -> String {
+    let mut h: HmacSha256 = HmacSha256::new_from_slice(&SECRET).unwrap();
+    h.update(s.as_bytes());
+    let result = h.finalize().into_bytes();
+
+
+    let b = result[result.len()-1] & 0xF;
+    let mut buffer: Vec<u8> = vec![0; 4];
+    let mut i = 0;
+    let mut j = 0;
+    loop {
+        i = j;
+        j = i + 1;
+        buffer[i] = result[i + b as usize];
+        if j > 3 {
+            buffer[0] = buffer[0] & 0xFF;
+            let mut rdr = Cursor::new(buffer);
+            let n = rdr.read_i32::<BigEndian>().unwrap();
+            return format!("{}", n);
+        }
+    }
+}
+
+fn calculate_app_id(body: &str,
+                    time: &chrono::NaiveDateTime) -> String {
+    let time_millis = time.timestamp_millis() as u64;
+    let ceil = (f64::from((time_millis / 1000) as u32) / f64::from(60.0)).ceil();
+    let s = format!("{}{}{}", USER_AGENT, app_version(), ceil);
+
+    return calculate_hmac(&s);
+}
+
+pub fn app_version() -> String {
+    let sdk_version = 30;
+    return format!("Homegate/12.6.0/12060003/Android/{}", sdk_version);
+}
+
+#[cfg(test)]
+mod test {
+    use std::fmt::Error;
+    use std::fs::File;
+    use std::io::Read;
+
+    use crate::api::app_id::calculate_app_id;
+
+    #[test]
+    fn test_app_id() -> Result<(), std::io::Error> {
+        let mut f = File::open("./resources/test/request-1.json")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        assert_eq!("1926888397", calculate_app_id(
+            &content,
+            &chrono::NaiveDateTime::new(
+                chrono::NaiveDate::from_ymd(2022, 1, 25),
+                chrono::NaiveTime::from_hms(1, 30, 56)),
+        ));
+        return Ok(());
+    }
+}
--- src/api/request.rs
+++ src/api/request.rs
@@ -1,33 +1,49 @@
-use reqwest::{Client, ClientBuilder, Response, Url, Error};
-use reqwest::header::HeaderValue;
-use crate::api::{BACKEND_URL, KEY};
+use reqwest::{Client, ClientBuilder, Error, Response, Url};
 use reqwest::header;
+use reqwest::header::HeaderValue;
+
+use crate::api::{API_PASSWORD, API_USERNAME, BACKEND_URL, USER_AGENT};
+use crate::api::app_id::app_version;
 
 fn build_client<'a>() -> Result<Client, Error> {
-    let client_builder : ClientBuilder = reqwest::Client::builder();
+    let client_builder: ClientBuilder = reqwest::Client::builder();
     let mut default_headers = header::HeaderMap::new();
+
+    let key = base64::encode(format!("{}:{}", API_USERNAME, API_PASSWORD));
+
+    let app_id = "486387659";
+
     default_headers.clear();
     default_headers.insert(header::AUTHORIZATION,
-                           HeaderValue::from_str(&format!("{}{}", "Basic ", KEY)).unwrap());
+                           HeaderValue::from_str(&format!("Basic {}", key)).unwrap());
     default_headers.insert(header::ACCEPT,
                            HeaderValue::from_static("application/json"));
+    default_headers.insert("X-App-Id", app_id.parse().unwrap());
+    default_headers.insert("X-App-Version", app_version().parse().unwrap());
+
     default_headers.insert(header::USER_AGENT,
-                           HeaderValue::from_static("okhttp/3.14.1"));
-    return client_builder.default_headers(default_headers).build()
+                           HeaderValue::from_static(USER_AGENT)); // Not a typo!
+    return client_builder.default_headers(default_headers).build();
 }
 
-pub async fn get(path: &str) -> Result<Response, Error>{
+pub async fn get(path: &str) -> Result<Response, Error> {
     let url = Url::parse(&format!("{}{}",
                                   BACKEND_URL,
                                   path));
 
-    let c : Client = build_client().unwrap();
+    let c: Client = build_client().unwrap();
     let req = c.get(url.unwrap()).build().unwrap();
     return c.execute(req).await;
 }
 
+pub async fn post_url(url: Url, body: &str) -> Result<Response, Error> {
+    let c: Client = build_client().unwrap();
+    let req = c.post(url).body(body.to_string()).build().unwrap();
+    return c.execute(req).await;
+}
+
 pub async fn get_url(url: Url) -> Result<Response, Error> {
-    let c : Client = build_client()?;
+    let c: Client = build_client()?;
     let req_b = c.get(url);
     let req = req_b.build()?;
     c.execute(req).await
@@ -35,17 +51,18 @@ pub async fn get_url(url: Url) -> Result<Response, Error> {
 
 #[cfg(tests)]
 pub mod tests {
-    use reqwest::{Response, Error, Url};
+    use reqwest::{Error, Response, Url};
+
     use crate::api::request;
     use crate::api::request::get_url;
 
     #[test]
     pub fn test_request() {
-        let r : Result<Response, Error> = request::get( "/rs/geo-areas?lan=en");
+        let r: Result<Response, Error> = request::get("/rs/geo-areas?lan=en");
         match r {
             Ok(mut response) => {
                 println!("{:?}", response.text().unwrap());
-            },
+            }
             Err(e) => {
                 println!("{:?}", e);
             }
